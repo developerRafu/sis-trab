d2f6244c028dc750e7e33a8b69e9e48ccb8f284b
rafael.henrique <rafael.henrique@techlead.com.br>
2022-07-26 23:23:28 -0300

    Initial commit

---
+++ src/main/java/com/rafu/sistrab/SisTrabApplication.java
@@ -0,0 +1,13 @@
+package com.rafu.sistrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisTrabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisTrabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafu/sistrab/SisTrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafu.sistrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisTrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
